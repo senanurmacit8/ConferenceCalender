ba7ecbf500377fa0fad6ab1f68d628e26f5d2050
senanurmacit <senamacit8@gmail.com>
2024-02-15 03:11:34 +0300

    swagger implemented

--- src/main/java/com/conference/calender/CalenderApplication.java
+++ src/main/java/com/conference/calender/CalenderApplication.java
@@ -2,7 +2,6 @@ package com.conference.calender;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
